1ce03430159ef292945f5be135d0040391b39377
Jyothiradithyan K <j.adithyan16@gmail.com>
2023-03-08 13:27:18 +0530

    arithmetic game

--- Assets/mathproblem.cs
+++ Assets/mathproblem.cs
@@ -14,7 +14,7 @@ public class mathproblem : MonoBehaviour
     public List<int> easyMathList = new List<int>();
 
     public int randomFirstNumber;
-    public int randomSecondtNumber;
+    public int randomSecondNumber;
     int firstNumberInProblem;
     int secondNumberInProblem;
     int answerOne;
@@ -26,13 +26,80 @@ public class mathproblem : MonoBehaviour
     public TextMeshProUGUI rightOrWrong_Text;
      
     public void DisplayMathProblem() {
-        
+        //generate a random numb as 1st and 2nd numb
+        randomFirstNumber = Random.Range(0,easyMathList.Count + 1);
+        randomSecondNumber = Random.Range(0,easyMathList.Count + 1);
+        //assign your first and second numb
+        firstNumberInProblem = randomFirstNumber;
+        secondNumberInProblem = randomSecondNumber;
+        //can use addition/sub/mult/div
+        answerOne = firstNumberInProblem + secondNumberInProblem;
+        displayRandomAnswer = Random.Range(0,2);
+        if(displayRandomAnswer == 0){
+            answerTwo = answerOne + Random.Range(1,5);
+        }
+        else{
+            answerTwo = answerOne - Random.Range(1,5);
+        }
+
+        firstnumber.text = ""+firstNumberInProblem;
+        secondnumber.text = ""+secondNumberInProblem;
+        randomAnswerPlacement = Random.Range(0,2);
+        if(randomAnswerPlacement == 0){
+
+            answer1.text = ""+ answerOne;
+            answer2.text = ""+ answerTwo;
+            currentAnswer = 0;
+        }
+        else{
+            answer1.text = ""+ answerTwo;
+            answer2.text = ""+ answerOne;
+            currentAnswer = 1;
+        }        
     }
+
+    public void ButtonAnswer1(){
+        if(currentAnswer == 0){
+
+            rightOrWrong_Text.enabled = true;
+            rightOrWrong_Text.color = Color.green;
+            rightOrWrong_Text.text = ("Correct");
+            Invoke("TurnOffText",1);
+        }
+        else{
+            rightOrWrong_Text.enabled = true;
+            rightOrWrong_Text.color = Color.red;
+            rightOrWrong_Text.text = ("Try Again!");
+            Invoke("TurnOffText",1);
+        }
+    }
+    public void ButtonAnswer2(){
+        if(currentAnswer == 1){
+
+            rightOrWrong_Text.enabled = true;
+            rightOrWrong_Text.color = Color.green;
+            rightOrWrong_Text.text = ("Correct");
+            Invoke("TurnOffText",1);
+        }
+        else{
+            rightOrWrong_Text.enabled = true;
+            rightOrWrong_Text.color = Color.red;
+            rightOrWrong_Text.text = ("Try Again!");
+            Invoke("TurnOffText",1);
+        }
+    }
+    public void TurnOffText(){
+
+        if(rightOrWrong_Text != null)
+            rightOrWrong_Text.enabled = false;
+        DisplayMathProblem();
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        DisplayMathProblem();
     }
 
     // Update is called once per frame
