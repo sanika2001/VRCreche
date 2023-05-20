8784a02ae0ef00d60b9884c16e1fc61625e7b571
Sanika P <porayilsanika@gmail.com>
2023-05-20 20:37:21 +0530

    Level-initial

--- Assets/mathproblem.cs
+++ Assets/mathproblem.cs
@@ -14,18 +14,24 @@ public class mathproblem : MonoBehaviour
     public TextMeshProUGUI answer1;
     public TextMeshProUGUI answer2;
     public TextMeshProUGUI answer3;
+    public TextMeshProUGUI ScoreVal;
 
     public List<int> easyMathList = new List<int>();
 
     public int randomFirstNumber;
     public int randomSecondNumber;
+    public int randomThirdNumber;
     int firstNumberInProblem;
     int secondNumberInProblem;
+    int thirdNumberInProblem;
     int answerOne;
     int answerTwo;
     int answerThree;
     int displayRandomAnswer;
     int randomAnswerPlacement;
+    int currentScore = 0;
+    int level = 1;
+    int levelWeight = 5;
     
     public int currentAnswer;
     public TextMeshProUGUI rightOrWrong_Text;
@@ -86,7 +92,8 @@ public class mathproblem : MonoBehaviour
 
     public void ButtonAnswer1(){
         if(currentAnswer == 0){
-
+            currentScore += levelWeight;
+            ScoreVal.text = "" + currentScore;
             rightOrWrong_Text.enabled = true;
             rightOrWrong_Text.color = Color.green;
             rightOrWrong_Text.text = ("Yay! You got it");
@@ -105,7 +112,8 @@ public class mathproblem : MonoBehaviour
     }
     public void ButtonAnswer2(){
         if(currentAnswer == 1){
-
+            currentScore += levelWeight;
+            ScoreVal.text = "" + currentScore;
             rightOrWrong_Text.enabled = true;
             rightOrWrong_Text.color = Color.green;
             rightOrWrong_Text.text = ("Yay! You got it");
@@ -126,7 +134,8 @@ public class mathproblem : MonoBehaviour
     {
         if (currentAnswer == 2)
         {
-
+            currentScore += levelWeight;
+            ScoreVal.text = "" + currentScore;
             rightOrWrong_Text.enabled = true;
             rightOrWrong_Text.color = Color.green;
             rightOrWrong_Text.text = ("Yay! You got it");
@@ -144,6 +153,66 @@ public class mathproblem : MonoBehaviour
             Invoke("TurnOffText", 1);
         }
     }
+
+
+    public void DisplayMathProblemLev2()
+    {
+        //generate a random numb as 1st and 2nd numb
+        randomFirstNumber = Random.Range(0, easyMathList.Count + 1);
+        randomSecondNumber = Random.Range(0, easyMathList.Count + 1);
+        randomThirdNumber = Random.Range(0, easyMathList.Count + 1);
+        //assign your first and second numb
+        firstNumberInProblem = randomFirstNumber;
+        secondNumberInProblem = randomSecondNumber;
+        thirdNumberInProblem = randomThirdNumber;
+        //can use addition/sub/mult/div
+        answerOne = firstNumberInProblem + secondNumberInProblem + thirdNumberInProblem;
+        displayRandomAnswer = Random.Range(0, 3);
+        if (displayRandomAnswer == 0)
+        {
+            answerTwo = answerOne + Random.Range(1, 10);
+            answerThree = answerOne - Random.Range(1, 5);
+        }
+        else if (displayRandomAnswer == 1)
+        {
+            answerTwo = answerOne - Random.Range(1, 5);
+            answerThree = answerOne + Random.Range(1, 5);
+        }
+        else
+        {
+            answerTwo = answerOne - Random.Range(1, 5);
+            answerThree = answerOne + Random.Range(1, 10);
+        }
+
+        firstnumber.text = "" + firstNumberInProblem;
+        secondnumber.text = "" + secondNumberInProblem;
+        randomAnswerPlacement = Random.Range(0, 3);
+        if (randomAnswerPlacement == 0)
+        {
+            answer1.text = "" + answerOne;
+            answer2.text = "" + answerTwo;
+            answer3.text = "" + answerThree;
+            currentAnswer = 0;
+        }
+        else if (randomAnswerPlacement == 0)
+        {
+            answer1.text = "" + answerThree;
+            answer2.text = "" + answerOne;
+            answer3.text = "" + answerTwo;
+            currentAnswer = 1;
+        }
+        else
+        {
+            answer1.text = "" + answerTwo;
+            answer2.text = "" + answerThree;
+            answer3.text = "" + answerOne;
+            currentAnswer = 2;
+        }
+    }
+
+
+
+
     public void TurnOffText(){
 
         if(rightOrWrong_Text != null)
